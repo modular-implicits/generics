4dcd6c34db7ea4f8588aa68b864bb055be27778d
Patrick Reader <_@pxeger.com>
2023-08-25 21:29:27 +0100

    more code cleanup

---
+++ lib/show.ml
@@ -0,0 +1,24 @@
+open Imp.Show
+open Generic
+
+implicit module ShowGenBasic {X : Show} : Show with type t = X.t basic = struct 
+    type t = X.t basic
+    let show (Basic (s, x)) = s ^ " " ^ X.show x
+end
+
+implicit module ShowGenProd {X : Show} {Y : Show} : Show with type t = (X.t, Y.t) prod = struct
+  type t = (X.t, Y.t) prod
+  let show (Prod (x, y)) = X.show x ^ ", " ^ Y.show y
+end
+
+implicit module ShowGenSum {X : Show} {Y : Show} : Show with type t = (X.t, Y.t) sum = struct
+  type t = (X.t, Y.t) sum
+  let show = function
+  | (Left a) -> X.show a
+  | (Right b) -> Y.show b
+end
+
+implicit module ShowGeneric {X : Generic} {XRep : Show with type t = X.rep} : Show with type t = X.t = struct
+  type t = X.t
+  let show x = XRep.show (X.toRep x)
+end
