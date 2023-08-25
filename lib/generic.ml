0edc7650a634165cf818bca51df838cc314334cc
Patrick Reader <_@pxeger.com>
2023-08-25 21:58:19 +0100

    add generic instances for unit and bool

--- lib/generic.ml
+++ lib/generic.ml
@@ -11,6 +11,28 @@ let fromRep {G : Generic} = G.fromRep
 
 type 'a basic = Basic of (string * 'a)
 
-type ('a, 'b) prod = Prod of ('a * 'b )
+type ('a, 'b) prod = Prod of ('a * 'b)
 
 type ('a, 'b) sum = Left of 'a | Right of 'b
+
+implicit module GenericUnit :
+  Generic with type t = unit and type rep = unit basic
+= struct
+  type t = unit
+  type rep = unit basic
+  let toRep () = Basic ("unit", ())
+  let fromRep _ = ()
+end
+
+implicit module GenericBool :
+  Generic with type t = bool and type rep = (unit basic, unit basic) sum
+= struct
+  type t = bool
+  type rep = (unit basic, unit basic) sum
+  let toRep = function
+    | false -> Left (Basic ("false", ()))
+    | true -> Right (Basic ("true", ()))
+  let fromRep = function
+    | Left _ -> false
+    | Right _ -> true
+end
